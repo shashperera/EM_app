bea31f881602f22452d096f94c5f03c82be05c8b
Shashi Perera <sanduni.p@sliit.lk>
2021-10-02 15:04:14 +0530

    Update App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { Text, StyleSheet, Button,View } from "react-native";
+
+
+const App: () => Node = () => {
+return(
+  <View>
+  <Text>Hi</Text>
+  <Button title="OK"/>
+  </View>
+
+);
+};
+
+export default App;
